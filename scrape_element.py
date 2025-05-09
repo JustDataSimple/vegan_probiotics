48600024666acd84637b66fb3f11dff3187a6c5f
JustDataSimple <JustDataSimple@proton.me>
2025-05-08 20:32:38 -0500

    added base code for scraping elements. will have to do another look through. next update will possible be in a draft branch.

--- scrape_element.py
+++ scrape_element.py
@@ -1,2 +1,9 @@
 from playwright.async_api import async_playwright
 import asyncio
+
+async def scrape_elements(page, target):
+    results = await page.query_selector_all(target)
+    for r in results:
+        text = await r.text_content()
+        print(f"Element text: {text}")
+    return results
